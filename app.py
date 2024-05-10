642774318be716136d0f4212a04bc49af56b47b9
uyunf <uyunragil0903@gmail.com>
2024-05-11 01:03:28 +0700

    Add requirements.txt and start.sh, update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from pymongo import MongoClient
 import jwt
 from datetime import datetime, timedelta
@@ -12,18 +15,22 @@ from flask import (
 )
 from werkzeug.utils import secure_filename
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
+
 app = Flask (__name__)
 
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['UPLOAD_FOLDER'] = './static/profile_pics'
 
 SECRET_KEY = 'SPARTA'
-
-MONGODB_CONNECTION_STRING = 'mongodb+srv://test:sparta@cluster0.zfjim4j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-
-client = MongoClient(MONGODB_CONNECTION_STRING)
-
-db = client.dbsparta_Plus_week5
 
 TOKEN_KEY = 'mytoken'
 
